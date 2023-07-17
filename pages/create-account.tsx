b8a777ba3134d4bf6e743eb90bc3a0e56e2c6884
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-17 13:31:11 +0900

    feat: basic sign up form

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,8 +1,39 @@
 import React from "react";
 import Layout from "@components/layout";
+import { useForm } from "react-hook-form";
 
-export default () => (
-  <Layout>
-    <h1>Create account</h1>
-  </Layout>
-);
+interface ISignUpForm {
+  username: string;
+  password: string;
+  passwordConfirm: string;
+}
+
+export default () => {
+  const { register } = useForm<ISignUpForm>();
+
+  return (
+    <Layout>
+      <h1>Sign Up</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input
+          {...register("username", { required: "true" })}
+          id="username"
+          placeholder="Username"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          {...register("password", { required: "true" })}
+          id="password"
+          type="password"
+        />
+        <label htmlFor="passwordconfirm">Password Confirm</label>
+        <input
+          {...register("passwordConfirm", { required: "true" })}
+          id="passwordconfirm"
+          type="password"
+        />
+      </form>
+    </Layout>
+  );
+};

90335d6657404a3f7832bdaec8af0dd26879d04c
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-17 10:48:25 +0900

    chore: add water.css and configure path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import { SWRConfig } from "swr";
-import "../global.css";
+//import "../global.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: any) {
   return (
     <SWRConfig
       value={{
-        fetcher: (url: string) => fetch(url).then((response) => response.json())
+        fetcher: (url: string) =>
+          fetch(url).then((response) => response.json()),
       }}
     >
-      <Component {...pageProps} />
+      <>
+        <Head>
+          <link //water.css
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </>
     </SWRConfig>
   );
 }

58a84a67c465af75b629e3d88d510fd62f596bc7
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-17 19:58:12 +0900

    feat: add TweetForm to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import Layout from "@components/layout";
+import { useForm } from "react-hook-form";
+import useUser from "@lib/client/useUser";
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    {[1, 2, 3, 4, 5].map((_, i) => (
-      <div key={i}>
-        <h3>Dummy user</h3>
-        <span>Dummy tweet</span>
-        <hr />
-      </div>
-    ))}
-  </Layout>
-);
+interface ITweetForm {
+  tweet: string;
+}
+
+export default () => {
+  //const { user } = useUser();
+  const { register } = useForm<ITweetForm>();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      {[1, 2, 3].map((_, i) => (
+        <div key={i}>
+          <h3>Dummy user</h3>
+          <span>Dummy tweet</span>
+          <hr />
+        </div>
+      ))}
+      <form>
+        <textarea
+          {...register("tweet", {
+            required: "true",
+            minLength: {
+              value: 5,
+              message: "Please write more than 5 letters",
+            },
+            maxLength: {
+              value: 300,
+              message: "Please write less than 300 letters",
+            },
+          })}
+        />
+        <button>Tweet!</button>
+      </form>
+    </Layout>
+  );
+};

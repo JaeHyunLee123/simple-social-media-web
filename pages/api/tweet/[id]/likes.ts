50c78ab6f247212bbd7c3bdf6ac1a794d7ed6583
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-18 15:59:02 +0900

    feat: tweet like button backend

--- pages/api/tweet/[id]/likes.ts
+++ pages/api/tweet/[id]/likes.ts
@@ -12,6 +12,42 @@ const handler = async (
     session: { user },
   } = req;
 
+  if (!(id && user))
+    return res.status(400).json({ ok: false, error: "requireEveryData" });
+
+  const alreadyExist = await prisma.like.findFirst({
+    where: {
+      userId: user.id,
+      tweetId: +id.toString(),
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (alreadyExist) {
+    await prisma.like.delete({
+      where: {
+        id: alreadyExist.id,
+      },
+    });
+  } else {
+    await prisma.like.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        tweet: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
+
   return res.status(200).json({ ok: true });
 };
 export default withApiSession(
--- pages/tweet/[id].tsx
+++ pages/tweet/[id].tsx
@@ -5,7 +5,7 @@ import { Tweet } from "@prisma/client";
 import { useRouter } from "next/router";
 import { formatDate } from "@lib/client/utils";
 import useMutation from "@lib/client/useMutation";
-import tweet from "pages/api/tweet";
+
 interface ITweetWithLikesAndUser extends Tweet {
   _count: { likes: number };
   user: { username: string };
@@ -25,17 +25,18 @@ export default () => {
   const [toggleLike, { isLoading }] = useMutation(
     router.query.id ? `/api/tweet/${router.query.id}/likes` : ""
   );
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(-1);
 
   useEffect(() => {
-    if (data) setLikes(data.tweet._count.likes);
+    if (data && likes === -1) setLikes(data.tweet._count.likes);
   }, [data]);
 
   const onLikeClick = () => {
     if (isLoading || !data) return;
     toggleLike({});
-    data.isLike ? setLikes((prev) => prev + 1) : setLikes((prev) => prev - 1);
     mutate({ ...data, isLike: !data.isLike }, false);
+    console.log(data.isLike);
+    data.isLike ? setLikes((prev) => prev - 1) : setLikes((prev) => prev + 1);
   };
 
   return (

53b3901c9e233cf3cb84c9c9fc7d00e19c001d00
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-22 18:19:27 +0900

    feat: make profile link to Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,15 +15,56 @@ const Layout = ({ children, isLogedIn = true }: LayoutProps) => {
     });
   };
   const isHome = router.pathname === "/";
+  const isProfile = router.pathname === "/profile";
 
   return (
     <div className="px-4 py-16 relative min-h-screen max-w-7xl mx-auto">
       <nav
         className={cls(
-          "flex justify-end items-center fixed top-0 left-0 w-full px-4 pt-5 text-lg bg-slate-50",
+          "flex justify-between items-center fixed top-0 left-0 w-full px-4 pt-5 text-lg bg-slate-50",
           "dark:bg-gray-900"
         )}
       >
+        <div className="flex space-x-2 items-center">
+          <Link href="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={cls(
+                "w-8 aspect-square cursor-pointer",
+                isHome ? "text-gray-50" : "text-gray-400"
+              )}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </Link>
+          <Link href="/profile">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={cls(
+                "w-8 aspect-square cursor-pointer",
+                isProfile ? "text-gray-50" : "text-gray-400"
+              )}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+          </Link>
+        </div>
         {isLogedIn ? (
           <button onClick={onLogoutClick}>LogOut</button>
         ) : (
@@ -34,26 +75,6 @@ const Layout = ({ children, isLogedIn = true }: LayoutProps) => {
         )}
       </nav>
       <div>{children}</div>
-      {isHome || !isLogedIn ? null : (
-        <nav className="flex justify-center fixed bottom-0 left-0 w-full pb-5">
-          <Link href="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-8 aspect-square"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
-          </Link>
-        </nav>
-      )}
     </div>
   );
 };

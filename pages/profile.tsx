7ee4bb1f4e7ba6bea73ab2083c8610956fa589a3
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2023-07-22 18:58:57 +0900

    feat: show posted tweets in profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@components/layout";
 import useUser from "@lib/client/useUser";
 import useSWR from "swr";
+import { Like, Tweet as ITweet, User } from "@prisma/client";
+import { cls, formatDate } from "@lib/client/utils";
+import Tweet from "@components/tweet";
+interface ILikeWithTweet extends Like {
+  tweet: ITweet;
+}
+
+interface ITweetWithLikesAndUser extends ITweet {
+  _count: { likes: number };
+  user: { username: string };
+}
+interface IProfileResponse {
+  ok: boolean;
+  user: User;
+  postedTweets: ITweetWithLikesAndUser[];
+  likingTweets: ILikeWithTweet[];
+}
 
 export default () => {
   useUser();
 
-  const {} = useSWR("/api/profile");
+  const [tweetsCategory, setTweetsCategory] = useState<"posted" | "liking">(
+    "posted"
+  );
+  const { data } = useSWR<IProfileResponse>("/api/profile");
 
   return (
     <Layout>
-      <h1>Proflile</h1>
+      <h1>{`${data?.user.username}'s proflile`}</h1>
+      <span>{`Signed at ${formatDate(
+        data?.user.createdAt.toString() || ""
+      )}`}</span>
+      <hr />
+      <div className="flex justify-around">
+        <button
+          onClick={() => {
+            setTweetsCategory("posted");
+          }}
+          className={cls(tweetsCategory === "posted" ? "text-red-500" : "")}
+        >
+          Posted
+        </button>
+        <button
+          onClick={() => {
+            setTweetsCategory("liking");
+          }}
+          className={cls(tweetsCategory === "liking" ? "text-red-500" : "")}
+        >
+          Liking
+        </button>
+      </div>
+      <div
+        className={cls(
+          "flex flex-col space-y-4",
+          "md:grid md:grid-cols-2 md:gap-2",
+          "lg:grid-cols-3"
+        )}
+      >
+        {tweetsCategory === "posted"
+          ? data?.postedTweets.map((tweet) => (
+              <Tweet tweet={tweet} key={tweet.id}></Tweet>
+            ))
+          : null}
+      </div>
     </Layout>
   );
 };
